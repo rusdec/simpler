5e602a3fc8b462d52f6a032ff909f3fbf03fa158
rusdec <ruskidecko@gmail.com>
2018-03-22 17:47:12 +0300

    Задание 1. Расшиеренные возможности метода Controller::render
    Реализуйте расширенные возможности метода render которые позволят возвращать ответ в других форматах, например:
    
    - render plain: "Plain text response"

--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -1,9 +1,14 @@
 require_relative 'view'
+require 'json'
 
 module Simpler
   class Controller
 
     attr_reader :name, :request, :response
+
+    CONTENT_TYPES = { html:   'text/html',
+                      json:   'text/json',
+                      plain:  'text/plain' }
 
     def initialize(env)
       @name = extract_name
@@ -15,8 +20,11 @@ module Simpler
       @request.env['simpler.controller'] = self
       @request.env['simpler.action'] = action
 
+      set_default_content_type
       set_default_headers
+
       send(action)
+
       write_response
 
       @response.finish
@@ -24,31 +32,84 @@ module Simpler
 
     private
 
+    attr_accessor :content_type, :content_body
+
     def extract_name
       self.class.name.match('(?<name>.+)Controller')[:name].downcase
-    end
-
-    def set_default_headers
-      @response['Content-Type'] = 'text/html'
     end
 
     def write_response
       body = render_body
 
+      set_content_type_header(content_type)
       @response.write(body)
     end
 
+    def set_default_headers
+      set_content_type_header(content_type)
+    end
+
     def render_body
-      View.new(@request.env).render(binding)
+      send "render_#{content_type}"
     end
 
     def params
       @request.params
     end
 
-    def render(template)
-      @request.env['simpler.template'] = template
+    def render(data)
+      data = parse_render_data(data)
+
+      if data[:type] == :html
+        @request.env['simpler.template'] = data
+      else
+        self.content_type = data[:type]
+        self.content_body = data[:body]
+      end
     end
 
+    def render_html
+      View.new(@request.env).render(binding)
+    end
+
+    def render_plain
+      content_body
+    end
+
+    def render_json
+      JSON.generate(content_body)
+    end
+
+    def set_default_content_type
+      self.content_type = :html
+    end
+
+    def set_content_type_header(type)
+      @response['Content-Type'] = CONTENT_TYPES[type]
+    end
+
+    def parse_render_data(data)
+      _data = { body: nil, type: :html }
+
+      if data.is_a?(Hash)
+        _data[:type] = data.keys[0]
+        _data[:body] = data[_data[:type]]
+      else
+        _data[:body] = data
+      end
+      content_type_valid!(_data[:type])
+
+      _data
+    end
+
+    def content_type_valid!(type)
+      controller_action = "#{self.class.name}##{@request.env['simpler.action']}"
+      raise "Unknown content type `#{type}` \
+             in #{controller_action}" unless content_type_valid?(type)
+    end
+
+    def content_type_valid?(type)
+      CONTENT_TYPES.has_key?(type)
+    end
   end
 end
